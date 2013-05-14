45c2812cb267800264f53d95226bc9eb7afaeed1
John <headscratch@gmail.com>
2013-05-13 23:55:05 -0600

    Put in File Slurping

--- converter.pl
+++ converter.pl
@@ -4,8 +4,24 @@ use Writer;
 use strict;
 use warnings;
 use FindBin;
-print "hi NAME\n";
 
+my $file_to_convert; 
+my $handle;
+my $headers;
+
+if (exists($ARGV[0])){
+	$file_to_convert = $ARGV[0];
+	}	
+else{
+	$file_to_convert = 'cal_report.csv';
+}
+
+open $handle, '<', $file_to_convert  or die "Can't open $file_to_convert. Please rename the csv to 'cal_report.csv' or enter the file name when running the script. : $!";
+chomp(my @lines = <$handle>);
+close $handle or die "Can't close $file_to_convert.: $!";
+$lines[0] =~ s/(\".??)(,)(.??\")&&/$1###############################$3/g;
+
+print $lines[0];
 my $rtf = RTF::Writer->new_to_file("greetings.rtf");
 $rtf->prolog( 'title' => "Greetings, hyoomon", 'fonts' => [ "Courier New", "Georgia"] );
 $rtf->number_pages;
